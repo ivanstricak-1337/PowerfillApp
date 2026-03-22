d414da8f41817d91a69fdc16fda6cca922ade0e9
ivanstricak-1337 <ivan.stricak@gmail.com>
2026-03-22 09:40:04 +0100

    Create ApiClient.kt

---
+++ app/src/main/java/com/powerfill/app/ApiClient.kt
@@ -0,0 +1,25 @@
+package com.powerfill.app
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
+object Config {
+    var BASE_URL = "https://YOUR-POWERFILL-URL/"
+    var TOKEN = "Bearer YOUR_API_TOKEN"
+
+    var DAY_PRICE = 0.25
+    var NIGHT_PRICE = 0.15
+
+    var DAY_START = 8
+    var DAY_END = 20
+    var NIGHT_START = 21
+    var NIGHT_END = 7
+}
+
+object ApiClient {
+    val api: PowerfillApi = Retrofit.Builder()
+        .baseUrl(Config.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(PowerfillApi::class.java)
+}
